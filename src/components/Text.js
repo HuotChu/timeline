7b6b10594f486ea9edcb17e67e3ff62da9107a4d
Pendem, Venkat (vp147v) <vp147v@us.att.com>
2021-05-06 09:15:51 -0500

    Size changes for Text story

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,9 +1,10 @@
 import './Text.css';
 
-export const createText = ({text})=>{
-  console.log(text);
+export const createText = ({text, primary = false})=>{
+  console.log(primary);
   const span = document.createElement('span');
-  span.className = 'Main-text'
+  span.className = primary ? 'Main-text' : 'sub-text';
+  console.log(span.className)
   span.innerHTML = text;
   
   return span;
