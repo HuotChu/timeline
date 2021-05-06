51f413f1b94a32f2f83eeed0e15c2eab9b4cfbe4
Bishop, Scott (sb167u) <sb167u@att.com>
2021-05-06 09:52:59 -0400

    Added icons, updated css and code style

--- src/components/date.js
+++ src/components/date.js
@@ -1,23 +1,19 @@
 import './date.css';
 
-export const createDateEle = ({
-  date,
-}) => {
+export const createDateEle = ({ date }) => {
   const span = document.createElement('span');
   const formattedDate = formatDate(date);
   span.innerText = formattedDate;
-
   span.className = ['timeline-date'];
   return span;
 
-  function formatDate(date){
+  function formatDate (date) {
     const dateArray = date.split('-');
-    if(dateArray.length == 3){
+    if (dateArray.length === 3) {
       dateArray.push(dateArray.splice(1,1)[0])
       return dateArray.reverse().join("/");
-    }else{
+    } else {
       return "Invalid Date";
     }
-
   }
 };
