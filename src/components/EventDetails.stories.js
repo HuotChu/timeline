e1d8fff54674fb3734317b9500e28148af8adfdd
Raquel, Prieto (rp928q) <rp928q@us.att.com>
2021-05-06 09:52:22 -0400

    details content

---
+++ src/components/EventDetails.stories.js
@@ -0,0 +1,43 @@
+import { createEventDetails } from './EventDetails';
+
+export default {
+  title: 'Example/EventDetails',
+  argTypes: {
+    label: { control: 'text' },
+    primary: { control: 'boolean' },
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select', options: ['small', 'medium', 'large'] },
+    },
+    onClick: { action: 'onClick' },
+  },
+};
+
+const Template = ({ label, ...args }) => {
+  // You can either use a function to create DOM elements or use a plain html string!
+  // return `<div>${label}</div>`;
+  return createEventDetails({ label, ...args });
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
