7cb333ad296d47d9172abb9dfe83edcb6abe1a18
Raquel, Prieto (rp928q) <rp928q@us.att.com>
2021-05-06 20:24:17 -0400

    data passed to create function fixes

--- src/components/activityView.js
+++ src/components/activityView.js
@@ -6,29 +6,28 @@ import { createButton } from './ExpandButton';
 import { createText } from './Text';
 
 export const createActivityView = ({ activityData, index = 0 }) => {
-    const { events } = getTimeLineData();
-    activityData = events[0] || {};
-    const dateComponent = createDateEle(activityData.date);
-    const title = createText(activityData.applicationId, true);
-    const subtitle = createText(activityData.shortDescription, false);
+    console.log(activityData);
+    const dateComponent = createDateEle({date: activityData.date});
+    const title = createText({text:activityData.applicationId, primary:true});
+    const subtitle = createText({text:activityData.shortDescription, primary: false});
     const buttonId = `expandButton_${index}`;
-    const expandButton = createButton(buttonId);
-    const eventDetails = createEventDetails(activityData);
+    const expandButton = createButton({elementId:buttonId});
+    const eventDetails = createEventDetails({details:activityData});
 
     const container = document.createElement('div');
     container.className = 'activity-container';
 
-    let iconType = ''; //==> switch on data to set class name
+    let iconType = activityData.type; //==> switch on data to set class name
 
     const view = `<div class='section-top'>
         <span class='activity-icon ${iconType}'></span>
         <span class='activity-title'>
-            <table><tr><td>${dateComponent}${title}</td></tr><tr><td>${subtitle}</td></tr></table>
+            <table><tr><td>${dateComponent.innerHTML}${title.innerHTML}</td></tr><tr><td>${subtitle.innerHTML}</td></tr></table>
         </span>
-        <span class='activity-button'>${expandButton}</span>
+        <span class='activity-button'>${expandButton.innerHTML}</span>
     </div>
     <div class='section-bottom hide'>
-        ${eventDetails}
+        ${eventDetails.innerHTML}
     </div>`;
 
     container.insertAdjacentHTML('afterbegin', view);
